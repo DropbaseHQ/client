5f14907c638232ae8bb93b2f559096e17dec627a
Ayazhan Zhakhan <az@dropbase.io>
2023-08-15 16:37:20 -0500

    updated run task

--- server/controllers/task/compose_task_code.py
+++ server/controllers/task/compose_task_code.py
@@ -9,15 +9,16 @@ from server.schemas.task import RunTask
 def compose_run_code(
     sqls: List[ReadSQLs], components: ReadComponents, functions: ReadFunctions, action: str
 ) -> str:
-    run_code = "from dataclasses import dataclass\n\n"
+    run_code = "from dataclasses import dataclass\n"
+    run_code += "import stripe\n\n"
     run_code += sqls[0].dataclass + "\n\n"
     run_code += components.dataclass + "\n\n"
 
     for func in functions:
         run_code += func.code + "\n\n"
 
-    run_code += """row = Row(**row_json)\n"""
-    run_code += """user_input=UserInput(**user_input_json)\n\n"""
+    run_code += """row = Row(**row)\n"""
+    run_code += """user_input=UserInput(**user_input)\n\n"""
 
     run_code += f"""result = {action}(user_input, row)"""
     return run_code
