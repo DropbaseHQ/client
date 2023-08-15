15039d1e4edb20f83445729e38c2c102fa4664ee
Jonathan <jon@dropbase.io>
2023-08-15 18:33:14 -0400

    Create code block atoms

--- client/src/features/app-builder/atoms/tableContextAtoms.ts
+++ client/src/features/app-builder/atoms/tableContextAtoms.ts
@@ -3,3 +3,7 @@ import {atom} from 'jotai'
 export const userInputAtom = atom({})
 
 export const selectedRowAtom = atom({})
+
+export const fetchersAtom = atom<any>({})
+
+export const  uiCodeAtom = atom('')
