ab0b79bd38307b2d3917451df948ffab69c7d09b
Daniel Chen <danielc@dropbase.io>
2023-08-18 16:04:44 -0400

    feat: add errors

--- client/src/features/smart-table/hooks/useUpdateSql.ts
+++ client/src/features/smart-table/hooks/useUpdateSql.ts
@@ -2,11 +2,13 @@ import { axios } from '@/lib/axios';
 import { useMutation } from 'react-query';
 
 const updateSql = async ({ sqlsId, code }: { sqlsId: string; code: string }) => {
-    const response = await axios.put(`/sqls/${sqlsId}`, { code });
-    return response.data;
-}
+	const response = await axios.put(`/sqls/${sqlsId}`, { code });
+	return response.data;
+};
 
-export const useUpdateSql = () => {
-    return useMutation(updateSql);
-
-}
+export const useUpdateSql = (options?: {
+	onSuccess?: (data: any) => void;
+	onError?: (error: any) => void;
+}) => {
+	return useMutation(updateSql, options);
+};
