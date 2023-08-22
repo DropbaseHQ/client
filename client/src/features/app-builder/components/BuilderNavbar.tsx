a4fb459b45453a30ab1a82d935d32a85d05a1261
Jonathan <jon@dropbase.io>
2023-08-22 18:21:20 -0400

    Move save studio into seperate hook

--- client/src/features/app-builder/components/BuilderNavbar.tsx
+++ client/src/features/app-builder/components/BuilderNavbar.tsx
@@ -1,68 +1,9 @@
 import { Flex, IconButton, Stack, Text, Button } from '@chakra-ui/react';
 import { ArrowLeft } from 'react-feather';
-import { useCreateAppFunction, useUpdateAppFunction } from '../hooks/useSaveAppCode';
-import { useCreateAppComponent, useUpdateAppComponent } from '../hooks/useSaveAppComponents';
-
-import { useParams } from 'react-router-dom';
-import { useGetApp } from '@/features/app/hooks';
-import { useAtom } from 'jotai';
-import { fetchersAtom, uiCodeAtom } from '../atoms/tableContextAtoms';
+import { useSaveStudio } from '../hooks/useSaveStudio';
 
 export const AppBuilderNavbar = () => {
-	const createAppFunction = useCreateAppFunction();
-	const updateAppFunction = useUpdateAppFunction();
-	const createAppComponent = useCreateAppComponent();
-	const updateAppComponent = useUpdateAppComponent();
-	const { appId } = useParams();
-	const {
-		fetchers: savedFetchers,
-		uiComponents,
-		refetch,
-		isLoading: appDetailsIsLoading,
-	} = useGetApp(appId || '');
-	const [fetchers] = useAtom(fetchersAtom);
-	const [uiCode] = useAtom(uiCodeAtom);
-	const handleSaveFunctions = async () => {
-		// UI components are currently saved as an array, but for now we only support one UI component
-		// Maybe we can just save it as an object instead of an array
-		const currentUIComponent = uiComponents?.[0];
-		if (currentUIComponent) {
-			await updateAppComponent.mutateAsync({
-				functionId: currentUIComponent.id || '',
-				code: uiCode || '',
-				// type: 'ui',
-			});
-		} else {
-			await createAppComponent.mutateAsync({
-				appId: appId || '',
-				code: uiCode || '',
-				// type: 'ui',
-			});
-		}
-
-		Object.entries(fetchers).forEach(async (fetcher: any) => {
-			const fetcherId = fetcher[0];
-			const fetcherCode = fetcher[1];
-
-			const IdInSavedFetchers = savedFetchers?.find((f: any) => f.id === fetcherId);
-
-			if (IdInSavedFetchers) {
-				await updateAppFunction.mutateAsync({
-					functionId: fetcherId || '',
-					code: fetcherCode || '',
-					type: 'fetcher',
-				});
-			} else {
-				await createAppFunction.mutateAsync({
-					appId: appId || '',
-					code: fetcherCode || '',
-					type: 'fetcher',
-				});
-			}
-		});
-		refetch();
-	};
-
+	const { saveStudio, isLoading: saveStudioIsLoading } = useSaveStudio();
 	return (
 		<Stack alignItems="center" h="10" borderBottomWidth="1px" direction="row">
 			<Flex h="full" alignItems="center">
@@ -84,12 +25,8 @@ export const AppBuilderNavbar = () => {
 				size="sm"
 				ml="auto"
 				mr="1rem"
-				onClick={handleSaveFunctions}
-				isLoading={
-					createAppFunction.isLoading ||
-					updateAppFunction.isLoading ||
-					appDetailsIsLoading
-				}
+				onClick={saveStudio}
+				isLoading={saveStudioIsLoading}
 			>
 				Save
 			</Button>
