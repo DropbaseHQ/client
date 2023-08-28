10e18c15e07bfecbcedfb8d0307c4b56ab01caec
Yash Joshi <yash@dropbase.io>
2023-08-28 18:19:33 +0530

    add inter font

--- client/src/lib/chakra-ui/theme/index.ts
+++ client/src/lib/chakra-ui/theme/index.ts
@@ -1,12 +1,13 @@
 import { theme as proTheme } from '@chakra-ui/pro-theme';
 import { extendTheme, theme as baseTheme } from '@chakra-ui/react';
 
-import '@fontsource-variable/open-sans';
-import '@fontsource-variable/spline-sans';
+import '@fontsource-variable/inter';
 
-export const theme = extendTheme(
-	{
-		colors: { ...baseTheme.colors, brand: baseTheme.colors.blue },
+export const theme = extendTheme({
+	...proTheme,
+	colors: { ...baseTheme.colors, brand: baseTheme.colors.blue },
+	fonts: {
+		heading: `Inter, apple-system, sans-serif`,
+		body: `Inter, apple-system, sans-serif`,
 	},
-	proTheme,
-);
+});
