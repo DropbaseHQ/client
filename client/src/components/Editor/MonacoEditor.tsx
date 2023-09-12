06680d84e16476a8553c4c87b9464c4233074b93
Yash Joshi <yash@dropbase.io>
2023-09-13 00:15:06 +0530

    auto resize monaco based on content

--- client/src/components/Editor/MonacoEditor.tsx
+++ client/src/components/Editor/MonacoEditor.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useState } from 'react';
 import Editor, { useMonaco } from '@monaco-editor/react';
 import { useMonacoTheme } from './hooks/useMonacoTheme';
 
@@ -5,5 +6,33 @@ export const MonacoEditor = (props: any) => {
 	const monaco = useMonaco();
 	useMonacoTheme(monaco);
 
-	return <Editor {...props} />;
+	const { onMount, height: defaultHeight, options, ...otherProps } = props;
+	const [height, setHeight] = useState(defaultHeight || 16);
+
+	const handleEditorMount = useCallback(
+		(editor: any, ...rest: any) => {
+			editor.onDidContentSizeChange(() => {
+				try {
+					setHeight(Math.min(1000, editor.getContentHeight()));
+					editor.layout();
+				} finally {
+					//
+				}
+			});
+
+			if (onMount) {
+				onMount(editor, ...rest);
+			}
+		},
+		[onMount],
+	);
+
+	return (
+		<Editor
+			{...otherProps}
+			options={{ ...(options || {}), scrollBeyondLastLine: false, automaticLayout: true }}
+			onMount={handleEditorMount}
+			height={height}
+		/>
+	);
 };
