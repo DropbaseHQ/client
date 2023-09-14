25d7e9319ebc77a574542e371d94cbc7ec7e969e
Trevor Du <trevor@dropbase.io>
2023-09-14 15:09:33 -0400

    call test_source before updating sources and add more tests

--- server/controllers/source/source.py
+++ server/controllers/source/source.py
@@ -5,7 +5,13 @@ from sqlalchemy.engine import URL
 from sqlalchemy.orm import Session
 
 from server import crud
-from server.schemas import SourceType, CreateSource, CreateSourceRequest, UpdateSource, UpdateSourceRequest
+from server.schemas import (
+    SourceType,
+    CreateSource,
+    CreateSourceRequest,
+    UpdateSource,
+    UpdateSourceRequest,
+)
 from server.utils.encrypt import _decrypt_db_creds, _encrypt_creds
 from server.utils.helper import raise_http_exception
 
@@ -17,14 +23,15 @@ SENSITIVE_FIELDS = ["password", "creds_json"]
 def get_source(db: Session, source_id: UUID):
     try:
         source = crud.source.get_object_by_id_or_404(db, source_id)
-        source.creds = _decrypt_db_creds(HARDCODED_WORKSPACE_ID, source.creds)
-        # TODO: remove sensitive data from source, like password and tokens
-        for key in SENSITIVE_FIELDS:
-            if key in source.creds:
-                source.creds[key] = None
-        return source
     except Exception as ex:
         raise_http_exception(404, f"source {source_id} not found", ex)
+
+    source.creds = _decrypt_db_creds(HARDCODED_WORKSPACE_ID, source.creds)
+    # TODO: remove sensitive data from source, like password and tokens
+    for key in SENSITIVE_FIELDS:
+        if key in source.creds:
+            source.creds[key] = None
+    return source
 
 
 def create_source(db: Session, request: CreateSourceRequest):
@@ -42,22 +49,23 @@ def create_source(db: Session, request: CreateSourceRequest):
 def update_source(db: Session, source_id: UUID, request: UpdateSourceRequest):
     try:
         source = crud.source.get_object_by_id_or_404(db, id=source_id)
-        source.creds = _decrypt_db_creds(HARDCODED_WORKSPACE_ID, source.creds)
-
-        for key in SENSITIVE_FIELDS:
-            if key in request.creds and request.creds[key] == "":
-                request.creds[key] = source.creds[key]
-
-        source_obj = UpdateSource(
-            name=request.name,
-            description=request.description,
-            type=request.type,
-            creds=_encrypt_creds(HARDCODED_WORKSPACE_ID, request.creds),
-            workspace_id=HARDCODED_WORKSPACE_ID,
-        )
-        return crud.source.update(db, db_obj=source, obj_in=source_obj)
     except Exception as ex:
         raise_http_exception(404, f"source {source_id} not found", ex)
+
+    source.creds = _decrypt_db_creds(HARDCODED_WORKSPACE_ID, source.creds)
+    for key in SENSITIVE_FIELDS:
+        if key in request.creds and request.creds[key] == "":
+            request.creds[key] = source.creds[key]
+
+    test_source(request)
+    source_obj = UpdateSource(
+        name=request.name,
+        description=request.description,
+        type=request.type,
+        creds=_encrypt_creds(HARDCODED_WORKSPACE_ID, request.creds),
+        workspace_id=HARDCODED_WORKSPACE_ID,
+    )
+    return crud.source.update(db, db_obj=source, obj_in=source_obj)
 
 
 def delete_source(db: Session, source_id: UUID):
@@ -67,24 +75,24 @@ def delete_source(db: Session, source_id: UUID):
         raise_http_exception(404, f"source {source_id} not found", ex)
 
 
-def test_source(source: CreateSourceRequest):
+def test_source(source: CreateSourceRequest | UpdateSourceRequest):
     if source.type == SourceType.POSTGRES:
-        test_source_creds_postgres(source)
+        test_source_creds_postgres(source.creds)
     else:
         # this is only thrown if we have added a source to SourceType but have
         # not added pre-creation tests to this if statement.
         raise_http_exception(501, f"Source type: \"{source.type}\" not implemented.")
 
 
-def test_source_creds_postgres(source: CreateSourceRequest):
+def test_source_creds_postgres(creds: dict):
     try:
         SQLALCHEMY_DATABASE_URL = URL.create(
             "postgresql",
-            username=source.creds.username,
-            password=source.creds.password,
-            host=source.creds.host,
-            port=source.creds.port,
-            database=source.creds.database,
+            username=creds["username"],
+            password=creds["password"],
+            host=creds["host"],
+            port=creds["port"],
+            database=creds["database"],
         )
         source_db = create_engine(SQLALCHEMY_DATABASE_URL)
         try:
--- server/tests/test_source.py
+++ server/tests/test_source.py
@@ -8,6 +8,29 @@ client = TestClient(app)
 from server.tests.conftest import ValueStorage
 
 
+MOCK_NONEXISTENT_UUID = "4d181f7d-d00a-4899-8235-1308ef20b46f"
+
+
+def test_create_source(mocker):
+    mocker.patch("server.controllers.source.source.test_source_creds_postgres")
+    data = {
+        "name": "test_pg_source",
+        "description": "this is from server/tests/test_source.py in function test_create_source",
+        "type": SourceType.POSTGRES,
+        "creds": {
+            "host": "test_host",
+            "port": 5432,
+            "username": "test_username",
+            "password": "test_password",
+            "database": "test_database",
+        },
+    }
+    response = client.post("/source/", json=data)
+    assert response.status_code == 200
+    assert response.json()["name"] == "test_pg_source"
+    ValueStorage.source_id = response.json()["id"]
+
+
 def test_create_source_invalid_creds():
     data = {
         "name": "test_pg_test_source",
@@ -44,32 +67,18 @@ def test_create_source_invalid_type(mocker):
     assert response.json()["detail"][0]["type"] == "type_error.enum"
 
 
-def test_create_source(mocker):
-    mocker.patch("server.controllers.source.source.test_source_creds_postgres")
-    data = {
-        "name": "test_pg_source",
-        "description": "this is from server/tests/test_source.py in function test_create_source",
-        "type": SourceType.POSTGRES,
-        "creds": {
-            "host": "test_host",
-            "port": 5432,
-            "username": "test_username",
-            "password": "test_password",
-            "database": "test_database",
-        },
-    }
-    response = client.post("/source/", json=data)
-    assert response.status_code == 200
-    assert response.json()["name"] == "test_pg_source"
-    ValueStorage.source_id = response.json()["id"]
-
-
 def test_read_source():
     response = client.get(f"/source/{ValueStorage.source_id}")
     assert response.status_code == 200
 
 
-def test_update_source():
+def test_read_source_not_found():
+    response = client.get(f"/source/{MOCK_NONEXISTENT_UUID}")
+    assert response.status_code == 404
+
+
+def test_update_source(mocker):
+    mocker.patch("server.controllers.source.source.test_source_creds_postgres")
     data = {
         "name": "test_pg_source_updated",
         "description": "this is from server/tests/test_source.py in function test_update_source",
@@ -87,7 +96,65 @@ def test_update_source():
     assert response.json()["name"] == "test_pg_source_updated"
 
 
+def test_update_source_not_found():
+    data = {
+        "name": "test_pg_source_updated",
+        "description": "this is from server/tests/test_source.py in function test_update_source",
+        "type": SourceType.POSTGRES,
+        "creds": {
+            "host": "test_host",
+            "port": 5432,
+            "username": "test_username",
+            "password": "",
+            "database": "test_database",
+        },
+    }
+    response = client.put(f"/source/{MOCK_NONEXISTENT_UUID}", json=data)
+    assert response.status_code == 404
+
+
+def test_update_source_invalid_creds():
+    data = {
+        "name": "test_pg_test_source",
+        "description": "this is from server/tests/test_source.py in function test_update_source_invalid_creds",
+        "type": SourceType.POSTGRES,
+        "creds": {
+            "host": "test_host",
+            "port": 5432,
+            "username": "test_username",
+            "password": "test_password",
+            "database": "test_database",
+        },
+    }
+    response = client.put(f"/source/{ValueStorage.source_id}", json=data)
+    assert response.status_code == 400
+
+
+def test_update_source_invalid_type(mocker):
+    mocker.patch("server.controllers.source.source.test_source_creds_postgres")
+    data = {
+        "name": "test_pg_source",
+        "description": "this is from server/tests/test_source.py in function test_update_source_invalid_type",
+        "type": "random invalid source type that has never before been seen",
+        "creds": {
+            "host": "test_host",
+            "port": 5432,
+            "username": "test_username",
+            "password": "test_password",
+            "database": "test_database",
+        },
+    }
+    response = client.put(f"/source/{ValueStorage.source_id}", json=data)
+    assert response.status_code == 422
+    assert response.json()["detail"][0]["type"] == "type_error.enum"
+
+
 def test_delete_source():
     response = client.delete(f"/source/{ValueStorage.source_id}")
     assert response.status_code == 200
     ValueStorage.source_id = None
+
+
+def test_delete_source_not_found():
+    response = client.delete(f"/source/{MOCK_NONEXISTENT_UUID}")
+    assert response.status_code == 404
