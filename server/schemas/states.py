4c8cb1cf7fd15a4d26b9fb8b3b435a7e2563a3ca
Ayazhan Zhakhan <az@dropbase.io>
2023-09-18 15:12:44 -0500

    split classes for components

--- server/schemas/states.py
+++ server/schemas/states.py
@@ -1,24 +1,46 @@
-from datetime import datetime
-from typing import Any, Dict, List, Literal, Optional, Union
-from uuid import UUID
+from typing import Any, List, Optional
 
 from pydantic import BaseModel
 
 
 # input
-class InputDisplayProperties(BaseModel):
+class ComponentDisplayProperties(BaseModel):
     message: Optional[str]
     message_type: Optional[str]
 
 
 class InputSharedProperties(BaseModel):
-    # editable
-    options: Optional[str]
     visible: Optional[bool]
     value: Optional[str]
 
 
-class InputStateProperties(InputDisplayProperties, InputSharedProperties):
+class SelectSharedProperties(BaseModel):
+    visible: Optional[bool]
+    value: Optional[str]
+    options: Optional[List[str:Any]]  # FIXME: {name : value, ...}
+
+
+class ButtonSharedProperties(BaseModel):
+    visible: Optional[bool]
+
+
+class TextSharedProperties(BaseModel):
+    visible: Optional[bool]
+
+
+class InputStateProperties(ComponentDisplayProperties, InputSharedProperties):
+    pass
+
+
+class SelectStateProperties(ComponentDisplayProperties, SelectSharedProperties):
+    pass
+
+
+class ButtonStateProperties(ComponentDisplayProperties, ButtonSharedProperties):
+    pass
+
+
+class TextStateProperties(TextSharedProperties):
     pass
 
 
