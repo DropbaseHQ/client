caae79a2105ea65b66df35a50d406a59d0359eb1
Yash Joshi <yash@dropbase.io>
2023-09-21 20:19:59 +0530

    update theme tokens

--- client/src/lib/chakra-ui/theme/index.ts
+++ client/src/lib/chakra-ui/theme/index.ts
@@ -6,7 +6,45 @@ import '@fontsource-variable/inter/wght.css';
 
 export const theme = extendTheme({
 	...proTheme,
-	colors: { ...baseTheme.colors, brand: baseTheme.colors.blue },
+	colors: {
+		...baseTheme.colors,
+		blue: {
+			'50': '#dae9fb',
+			'100': '#b5d4f7',
+			'200': '#90bef3',
+			'300': '#6ba9ef',
+			'400': '#4693ec',
+			'500': '#1775E1',
+			'600': '#156acb',
+			'700': '#1157a6',
+			'800': '#0e4481',
+			'900': '#0a305c',
+		},
+		gray: {
+			'50': '#F8F9FA',
+			'100': '#E9ECEF',
+			'200': '#DEE2E6',
+			'300': '#CED4DA',
+			'400': '#ADB5BD',
+			'500': '#6C757D',
+			'600': '#495057',
+			'700': '#343A40',
+			'800': '#212529',
+			'900': '#161616',
+		},
+		brand: {
+			'50': '#dae9fb',
+			'100': '#b5d4f7',
+			'200': '#90bef3',
+			'300': '#6ba9ef',
+			'400': '#4693ec',
+			'500': '#1775E1',
+			'600': '#156acb',
+			'700': '#1157a6',
+			'800': '#0e4481',
+			'900': '#0a305c',
+		},
+	},
 	semanticTokens: {
 		...((proTheme?.semanticTokens || {}) as any),
 		colors: {
