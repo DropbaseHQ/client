42a61d93b6859cb24e65d7e396bbf6e1858e575a
Ayazhan Zhakhan <az@dropbase.io>
2023-09-22 11:49:27 -0500

    updated cors for prod

--- server/main.py
+++ server/main.py
@@ -15,14 +15,7 @@ app = FastAPI()
 
 # app.middleware("http")(catch_exceptions_middleware)
 
-# origins = ["https://dropbase.io"]
-origins = [
-    "http://127.0.0.1:3000",
-    "https://dev.dropbase.io",
-    "https://www.dev.dropbase.io",
-    "http://localhost:3000",
-    "http://www.localhost:3000",
-]
+origins = ["http://127.0.0.1:3000", "https://app.dropbase.io", "https://www.app.dropbase.io"]
 
 
 ### ROUTES ###
