cad5234fe7c98de000f585b455167a7ca63175fb
Ayazhan Zhakhan <az@dropbase.io>
2023-09-26 15:40:24 -0500

    fixed UserRole imports

--- server/crud/role/crud_role.py
+++ server/crud/role/crud_role.py
@@ -4,19 +4,19 @@ from uuid import UUID
 from sqlalchemy.orm import Session
 
 from server.crud.base import CRUDBase
-from server.models import Role
+from server.models import UserRole
 from server.schemas.role import CreateRole, UpdateRole
 
 
-class CRUDRole(CRUDBase[Role, CreateRole, UpdateRole]):
+class CRUDRole(CRUDBase[UserRole, CreateRole, UpdateRole]):
     def user_is_in_workspace(self, db: Session, user_id: UUID, workspace_id: UUID) -> bool:
         return (
-            db.query(Role)
-            .filter(Role.user_id == str(user_id))
-            .filter(Role.workspace_id == str(workspace_id))
+            db.query(UserRole)
+            .filter(UserRole.user_id == user_id)
+            .filter(UserRole.workspace_id == workspace_id)
             .count()
             > 0
         )
 
 
-role = CRUDRole(Role)
+role = CRUDRole(UserRole)
