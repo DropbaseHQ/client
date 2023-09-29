ce210f31a42b6203d16fe997f43f5fe904364f06
Yash Joshi <yash@dropbase.io>
2023-09-30 00:29:58 +0530

    feat: add display rules

---
+++ client/src/lib/chakra-ui/theme/components/select.ts
@@ -0,0 +1,27 @@
+export default {
+	parts: ['field', 'icon'],
+	sizes: {
+		md: {
+			field: {
+				height: '11',
+				paddingInlineEnd: '8',
+			},
+			group: {
+				height: '11',
+			},
+		},
+		sm: {
+			field: {
+				height: '9',
+				paddingInlineEnd: '8',
+			},
+			group: {
+				height: '9',
+			},
+		},
+	},
+	defaultProps: {
+		size: 'md',
+		variant: 'outline',
+	},
+};
