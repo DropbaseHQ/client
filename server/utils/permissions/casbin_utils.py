05c8a7d18c444042bb199ca3611da75630d85113
jonDropbase <jon@dropbase.io>
2023-09-28 20:23:20 -0400

    Specific policy loading and priority implement

--- server/utils/permissions/casbin_utils.py
+++ server/utils/permissions/casbin_utils.py
@@ -1,4 +1,5 @@
 import casbin
+from casbin import persist
 from uuid import UUID
 from sqlalchemy.orm import Session
 from server import crud
@@ -10,19 +11,33 @@ adapter = Adapter(SQLALCHEMY_DATABASE_URL, db_class=Policy)
 enforcer = casbin.Enforcer("server/utils/permissions/casbin_model.conf", adapter, True)
 
 
+def load_specific_policies(policies):
+    for policy in policies:
+        print("policy", policy)
+        persist.load_policy_line(str(policy), enforcer.model)
+
+
+def unload_specific_policies(policies):
+    for policy in policies:
+        unload_policy_line(str(policy), enforcer.model)
+
+
 def enforce_action(db, user_id, workspace_id, resource, action):
     role = crud.user_role.get_user_role(db, user_id, workspace_id)
-    policies = crud.role.get_role_policies(db, role.id)
-    formatted_policies = [str(policy).split(", ")[1:] for policy in policies]
 
     try:
-        enforcer.add_policies(formatted_policies)
+        if not role.is_default:
+            policies = crud.role.get_role_policies(db, role.id)
+            formatted_policies = [str(policy) for policy in policies]
+            load_specific_policies(formatted_policies)
+        policies = enforcer.get_policy()
         return enforcer.enforce(role.name, resource, action)
 
     except Exception as e:
         print("Permission enforcement error", e)
     finally:
-        enforcer.remove_policies(formatted_policies)
+        unload_specific_policies(formatted_policies)
+        policies = enforcer.get_policy()
 
 
 def add_policy(db: Session, role_id: UUID, resource, action):
@@ -42,3 +57,50 @@ def update_policy(db, policy_id, role=None, resource=None, action=None):
         policy.v2 = action
     db.commit()
     return policy
+
+
+def unload_policy_line(line, model):
+    """unloads a text line by removing a policy rule from the model."""
+
+    if line == "":
+        return
+
+    if line[:1] == "#":
+        return
+
+    stack = []
+    tokens = []
+    for c in line:
+        if c == "[" or c == "(":
+            stack.append(c)
+            tokens[-1] += c
+        elif c == "]" or c == ")":
+            stack.pop()
+            tokens[-1] += c
+        elif c == "," and len(stack) == 0:
+            tokens.append("")
+        else:
+            if len(tokens) == 0:
+                tokens.append(c)
+            else:
+                tokens[-1] += c
+
+    tokens = [x.strip() for x in tokens]
+
+    key = tokens[0]
+    sec = key[0]
+
+    if sec not in model.model.keys():
+        return
+
+    if key not in model.model[sec].keys():
+        return
+
+    policy_to_remove = tokens[1:]
+
+    # Find and remove the policy rule from the model's memory
+    try:
+        model.model[sec][key].policy.remove(policy_to_remove)
+    except ValueError:
+        # Handle the case where the policy rule is not found
+        pass
