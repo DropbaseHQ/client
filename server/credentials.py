7f9e22d00fbb53a8b7178557c8483abc29a64bd4
Trevor Du <trevor@dropbase.io>
2023-10-03 15:04:19 -0400

    try again

--- server/credentials.py
+++ server/credentials.py
@@ -10,6 +10,7 @@ POSTGRES_DB_NAME = os.environ.get("POSTGRES_DB_NAME")
 POSTGRES_DB_USER = os.environ.get("POSTGRES_DB_USER")
 POSTGRES_DB_PASS = os.environ.get("POSTGRES_DB_PASS")
 
+print("TESTTESTSTEST")
 print(POSTGRES_DB_HOST.split())
 
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
