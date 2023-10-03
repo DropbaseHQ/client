34d42bde3b34414a2809095b4b598553d076033c
Yash Joshi <yash@dropbase.io>
2023-10-03 23:33:36 +0530

    prevent pinned filters in preview mode

--- client/src/features/app/components/NewApp.tsx
+++ client/src/features/app/components/NewApp.tsx
@@ -1,4 +1,6 @@
+import { useSetAtom } from 'jotai';
 import { Stack } from '@chakra-ui/react';
+import { useEffect } from 'react';
 import { Panel, PanelGroup } from 'react-resizable-panels';
 
 import { useInitPage } from '@/features/new-page';
@@ -8,9 +10,23 @@ import { StackedTables } from '@/features/new-smart-table';
 import { Loader } from '@/components/Loader';
 
 import { AppNavbar } from './AppNavbar';
+import { appModeAtom } from '@/features/app/atoms';
 
 export const NewApp = () => {
+	const updateMode = useSetAtom(appModeAtom);
 	const { isLoading } = useInitPage();
+
+	useEffect(() => {
+		updateMode({
+			isPreview: true,
+		});
+
+		return () => {
+			updateMode({
+				isPreview: false,
+			});
+		};
+	}, [updateMode]);
 
 	return (
 		<Stack spacing="0" h="full">
