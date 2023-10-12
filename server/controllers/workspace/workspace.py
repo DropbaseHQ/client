c74f3dbdecc211de5d81b78c3a1b2b7a6f79d962
jonDropbase <jon@dropbase.io>
2023-10-12 17:11:22 -0400

    Fix: Add commit to add user to workspace

--- server/controllers/workspace/workspace.py
+++ server/controllers/workspace/workspace.py
@@ -12,25 +12,29 @@ def get_workspace_groups(db, workspace_id):
 
 
 def add_user_to_workspace(db, workspace_id, user_email, role_id):
-    user = crud.user.get_user_by_email(db, user_email)
-    if not user:
-        raise Exception("User does not exist")
-    workspace_user = crud.user_role.create(
-        db,
-        obj_in={"user_id": user.id, "workspace_id": workspace_id, "role_id": role_id},
-        auto_commit=False,
-    )
+    try:
+        user = crud.user.get_user_by_email(db, user_email)
+        if not user:
+            raise Exception("User does not exist")
+        workspace_user = crud.user_role.create(
+            db,
+            obj_in={"user_id": user.id, "workspace_id": workspace_id, "role_id": role_id},
+            auto_commit=False,
+        )
 
-    target_role = crud.role.get_object_by_id_or_404(db, id=role_id)
-    crud.policy.create(
-        db,
-        obj_in={
-            "ptype": "g",
-            "v0": user.id,
-            "v1": target_role.name,
-            "workspace_id": workspace_id,
-        },
-        auto_commit=False,
-    )
-
-    return workspace_user
+        target_role = crud.role.get_object_by_id_or_404(db, id=role_id)
+        crud.policy.create(
+            db,
+            obj_in={
+                "ptype": "g",
+                "v0": user.id,
+                "v1": target_role.name,
+                "workspace_id": workspace_id,
+            },
+            auto_commit=False,
+        )
+        db.commit()
+        return workspace_user
+    except Exception as e:
+        db.rollback()
+        raise e
