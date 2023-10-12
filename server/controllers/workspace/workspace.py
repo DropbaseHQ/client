4ad436076c0b5c87b54dba1b439392f31a252e96
jonDropbase <jon@dropbase.io>
2023-10-12 15:05:40 -0400

    Fix adding workspace members role

--- server/controllers/workspace/workspace.py
+++ server/controllers/workspace/workspace.py
@@ -16,6 +16,21 @@ def add_user_to_workspace(db, workspace_id, user_email, role_id):
     if not user:
         raise Exception("User does not exist")
     workspace_user = crud.user_role.create(
-        db, obj_in={"user_id": user.id, "workspace_id": workspace_id, "role_id": role_id}
+        db,
+        obj_in={"user_id": user.id, "workspace_id": workspace_id, "role_id": role_id},
+        auto_commit=False,
     )
+
+    target_role = crud.role.get_object_by_id_or_404(db, id=role_id)
+    crud.policy.create(
+        db,
+        obj_in={
+            "ptype": "g",
+            "v0": user.id,
+            "v1": target_role.name,
+            "workspace_id": workspace_id,
+        },
+        auto_commit=False,
+    )
+
     return workspace_user
