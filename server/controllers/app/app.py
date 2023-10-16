1b96432dca5bb1d3950fa919e980279ccbfa393c
jonDropbase <jon@dropbase.io>
2023-10-16 13:06:49 -0400

    Return only apps that user is allowed to use

--- server/controllers/app/app.py
+++ server/controllers/app/app.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import Session
 from server import crud
 from server.models import User
 from server.schemas import CreateApp, ReadApp, ReadPage
+from server.utils.permissions.casbin_utils import get_contexted_enforcer
 
 
 def get_user_apps(db: Session, user: User, workspace_id: UUID):
@@ -16,9 +17,16 @@ def get_user_apps(db: Session, user: User, workspace_id: UUID):
     else:
         workspace = crud.workspace.get_object_by_id_or_404(db, id=workspace_id)
     apps = crud.app.get_workspace_apps(db, workspace_id=workspace.id)
+    enforcer = get_contexted_enforcer(db, workspace.id)
+    allowed_apps = []
     for app in apps:
-        app.pages = crud.page.get_app_pages(db, app.id)
-    return apps
+        if enforcer.enforce(str(user.id), str(app.id), "use") or enforcer.enforce(
+            str(user.id), "app", "use"
+        ):
+            app.pages = crud.page.get_app_pages(db, app.id)
+            allowed_apps.append(app)
+
+    return allowed_apps
 
 
 def create_app(db: Session, request: CreateApp, user: User):
