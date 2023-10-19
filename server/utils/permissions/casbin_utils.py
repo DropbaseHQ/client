3349016e2f2049e3e4135e08dbfab489f3b44350
jonDropbase <jon@dropbase.io>
2023-10-19 17:30:30 -0400

    Debug: Change policy loading method

--- server/utils/permissions/casbin_utils.py
+++ server/utils/permissions/casbin_utils.py
@@ -34,29 +34,20 @@ def get_contexted_enforcer(db, workspace_id):
 
     # Load workspace policies
     policies = crud.workspace.get_workspace_policies(db, workspace_id)
-    formatted_policies = [str(policy) for policy in policies]
-    load_specific_policies(formatted_policies)
+    formatted_policies = [str(policy).split(", ")[1:] for policy in policies]
+    enforcer.add_policies(formatted_policies)
+    # load_specific_policies(formatted_policies)
 
     # Load grouping policies
     grouping_policies = crud.workspace.get_workspace_grouping_policies(db, workspace_id)
-    formatted_groups = [str(policy).split(", ")[1:] for policy in grouping_policies]
+    formatted_groups = [str(g_policy).split(", ")[1:] for g_policy in grouping_policies]
     enforcer.add_grouping_policies(formatted_groups)
 
     return enforcer
 
 
 def enforce_action(db, user_id, workspace_id, resource, action, resource_crud, resource_id=None):
-    # Refreshes policy. Allows dynamic policy changes while deployed.
-    enforcer.load_policy()
-    # Load workspace policies
-    policies = crud.workspace.get_workspace_policies(db, workspace_id)
-    formatted_policies = [str(policy) for policy in policies]
-    load_specific_policies(formatted_policies)
-
-    # Load grouping policies
-    grouping_policies = crud.workspace.get_workspace_grouping_policies(db, workspace_id)
-    formatted_groups = [str(policy).split(", ")[1:] for policy in grouping_policies]
-    enforcer.add_grouping_policies(formatted_groups)
+    enforcer = get_contexted_enforcer(db, workspace_id)
 
     try:
         if resource_id:
