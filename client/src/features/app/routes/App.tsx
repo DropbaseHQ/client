3e574a2ad9d947309d6fec4922f2528745c94b42
Yash Joshi <yash@dropbase.io>
2023-10-25 22:21:27 +0530

    only block app route from token validation

--- client/src/features/app/routes/App.tsx
+++ client/src/features/app/routes/App.tsx
@@ -1,9 +1,28 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
+import { Center, Progress, Stack, Text } from '@chakra-ui/react';
 
 import { NewAppBuilder } from '@/features/new-app-builder';
 import { NewApp } from '../components';
+import { useSyncProxyToken } from '@/features/settings/hooks/token';
 
 export const App = () => {
+	const { isLoading: isLoadingTokens, isValid: isValidToken } = useSyncProxyToken();
+
+	if (isLoadingTokens) {
+		return (
+			<Center as={Stack} spacing="6" w="full" h="full">
+				<Text color="heading" fontSize="lg" fontWeight="medium">
+					Validating token...
+				</Text>
+				<Progress minW="sm" size="xs" isIndeterminate />
+			</Center>
+		);
+	}
+
+	if (!isValidToken) {
+		return <Navigate to="/settings/developer" />;
+	}
+
 	return (
 		<Routes>
 			<Route index element={<NewAppBuilder />} />
