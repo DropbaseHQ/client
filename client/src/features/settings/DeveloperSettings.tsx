8bc5beb30f543909e7c46be3f667dbe08626a564
Yash Joshi <yash@dropbase.io>
2023-10-26 18:54:14 +0530

    update copy icon

--- client/src/features/settings/DeveloperSettings.tsx
+++ client/src/features/settings/DeveloperSettings.tsx
@@ -8,9 +8,12 @@ import {
 	Alert,
 	AlertIcon,
 	AlertTitle,
+	IconButton,
+	useClipboard,
 } from '@chakra-ui/react';
 import { useAtom, useAtomValue } from 'jotai';
-import { CheckCircle, Circle } from 'react-feather';
+import { CheckCircle, Circle, Copy } from 'react-feather';
+import copy from 'copy-to-clipboard';
 import { PageLayout } from '@/layout';
 import { workspaceAtom } from '@/features/workspaces';
 import { useGetCurrentUser } from '@/features/authorization/hooks/useGetUser';
@@ -23,6 +26,7 @@ export const DeveloperSettings = () => {
 	const { user } = useGetCurrentUser();
 	const { isLoading, tokens } = useProxyTokens({ userId: user.id, workspaceId });
 	const [selectedToken, setToken] = useAtom(proxyTokenAtom);
+	const { hasCopied, onCopy } = useClipboard('test');
 
 	const toast = useToast();
 
@@ -65,14 +69,13 @@ export const DeveloperSettings = () => {
 								direction="row"
 								key={token}
 								cursor="pointer"
-								textOverflow="ellipsis"
 								overflow="hidden"
 								borderWidth="1px"
 								borderColor={isSelected ? 'blue.500' : 'gray.200'}
 								borderRadius="sm"
 								bg="white"
 								alignItems="center"
-								p="6"
+								p="4"
 								as="button"
 								onClick={() => {
 									setToken(token);
@@ -86,11 +89,38 @@ export const DeveloperSettings = () => {
 								}}
 							>
 								<Icon
+									flexShrink="0"
 									color={isSelected ? 'blue.500' : 'gray.500'}
 									as={isSelected ? CheckCircle : Circle}
 									boxSize={5}
 								/>
-								<Text fontSize="lg">{token}</Text>
+								<Text
+									w="full"
+									whiteSpace="nowrap"
+									overflow="hidden"
+									flex="1"
+									textOverflow="ellipsis"
+									fontSize="lg"
+								>
+									{token}
+								</Text>
+								<IconButton
+									flexShrink="0"
+									variant="ghost"
+									icon={
+										hasCopied ? <CheckCircle size="14" /> : <Copy size="14" />
+									}
+									size="sm"
+									onClick={() => {
+										onCopy();
+										copy(token);
+										toast({
+											title: 'Token copied',
+											status: 'success',
+										});
+									}}
+									aria-label="Copy token"
+								/>
 							</Stack>
 						);
 					})}
