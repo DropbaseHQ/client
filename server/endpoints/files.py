a73eff52ab3e4f26a09c40eedb4f82d6b66dcc8d
Ayazhan Zhakhan <az@dropbase.io>
2023-10-27 13:16:26 -0500

    added create/edit/delete files endpoint

--- server/endpoints/files.py
+++ server/endpoints/files.py
@@ -4,17 +4,29 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from server import crud
+from server.schemas.files import CreateFiles, UpdateFiles
 from server.utils.authorization import RESOURCES, AuthZDepFactory
 from server.utils.connect import get_db
 
 files_authorizer = AuthZDepFactory(default_resource_type=RESOURCES.FILES)
-router = APIRouter(
-    prefix="/files",
-    tags=["files"],
-    dependencies=[Depends(files_authorizer)],
-)
+router = APIRouter(prefix="/files", tags=["files"], dependencies=[Depends(files_authorizer)])
 
 
 @router.get("/{page_id}")
 def get_page_files(page_id: UUID, db: Session = Depends(get_db)):
     return crud.files.get_page_files(db, page_id=page_id)
+
+
+@router.post("/")
+def create_file(request: CreateFiles, db: Session = Depends(get_db)):
+    return crud.files.create(db, obj_in=request)
+
+
+@router.put("/{file_id}")
+def update_components(file_id: UUID, request: UpdateFiles, db: Session = Depends(get_db)):
+    return crud.files.update_by_pk(db, pk=file_id, obj_in=request)
+
+
+@router.delete("/{file_id}")
+def delete_components(file_id: UUID, db: Session = Depends(get_db)):
+    return crud.files.remove(db, id=file_id)
