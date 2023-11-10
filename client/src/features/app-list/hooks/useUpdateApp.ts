fcff1192eab7a229ddd46ce605817eb354a86e51
jonDropbase <jon@dropbase.io>
2023-11-09 21:36:39 -0500

    Fix: Updating app name invalidates page query

--- client/src/features/app-list/hooks/useUpdateApp.ts
+++ client/src/features/app-list/hooks/useUpdateApp.ts
@@ -1,6 +1,7 @@
 import { useMutation, useQueryClient } from 'react-query';
 import { workerAxios } from '@/lib/axios';
 import { APPS_QUERY_KEY } from './useGetWorkspaceApps';
+import { PAGE_DATA_QUERY_KEY } from '@/features/page';
 
 const updateApp = async ({ appId, oldName, newName }: any) => {
 	const response = await workerAxios.put(`/app/${appId}`, {
@@ -16,6 +17,7 @@ export const useUpdateApp = (props: any = {}) => {
 		...props,
 		onSettled: () => {
 			queryClient.invalidateQueries(APPS_QUERY_KEY);
+			queryClient.invalidateQueries(PAGE_DATA_QUERY_KEY);
 		},
 	});
 };
