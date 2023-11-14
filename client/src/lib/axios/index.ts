ddd385d559e3bfc34d30daba3e31382ff4b20a64
jonDropbase <jon@dropbase.io>
2023-11-14 18:58:21 -0500

    Fix: Remove debug code

--- client/src/lib/axios/index.ts
+++ client/src/lib/axios/index.ts
@@ -6,7 +6,7 @@ export const axios = Axios.create({
 });
 
 export const workerAxios = Axios.create({
-	baseURL: `${import.meta.env.VITE_WORKER_API_ENDPOINT}`,
+	baseURL: `${import.meta.env.VITE_WORKER_API_ENDPOINT}/worker`,
 });
 
 axios.interceptors.response.use(
