bc271037d61936a1f62d9b8d6f7ab5a34c2dac84
jonDropbase <95246696+jonDropbase@users.noreply.github.com>
2023-11-15 15:35:06 -0500

    Enable auth for worker endpoints (#70)

--- server/endpoints/worker/file.py
+++ server/endpoints/worker/file.py
@@ -6,12 +6,18 @@ from sqlalchemy.orm import Session
 from server import crud
 from server.schemas.files import CreateFiles, RenameFile, UpdateFiles
 from server.utils.connect import get_db
+from server.utils.authorization import RESOURCES, AuthZDepFactory
 
-router = APIRouter(prefix="/files", tags=["files"])
+files_authorizer = AuthZDepFactory(default_resource_type=RESOURCES.FILES)
+router = APIRouter(
+    prefix="/files", tags=["files"], dependencies=[Depends(files_authorizer)]
+)
 
 
 @router.post("/")
-def create_file(request: CreateFiles, response: Response, db: Session = Depends(get_db)):
+def create_file(
+    request: CreateFiles, response: Response, db: Session = Depends(get_db)
+):
     try:
         return crud.files.create(db, obj_in=request)
     except Exception as e:
@@ -21,7 +27,9 @@ def create_file(request: CreateFiles, response: Response, db: Session = Depends(
 
 @router.put("/rename")
 def update_name(request: RenameFile, db: Session = Depends(get_db)):
-    file = crud.files.get_page_file_by_name(db, page_id=request.page_id, file_name=request.old_name)
+    file = crud.files.get_page_file_by_name(
+        db, page_id=request.page_id, file_name=request.old_name
+    )
     file.name = request.new_name
     db.commit()
     return file
@@ -29,14 +37,18 @@ def update_name(request: RenameFile, db: Session = Depends(get_db)):
 
 @router.put("/source")
 def update_source(request: CreateFiles, db: Session = Depends(get_db)):
-    file = crud.files.get_page_file_by_name(db, page_id=request.page_id, file_name=request.name)
+    file = crud.files.get_page_file_by_name(
+        db, page_id=request.page_id, file_name=request.name
+    )
     file.source = request.source
     db.commit()
     return file
 
 
 @router.put("/{file_id}")
-def update_file_request(file_id: UUID, request: UpdateFiles, db: Session = Depends(get_db)):
+def update_file_request(
+    file_id: UUID, request: UpdateFiles, db: Session = Depends(get_db)
+):
     return crud.files.update_by_pk(db, pk=file_id, obj_in=request)
 
 
