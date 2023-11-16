e95a84ee8ba3e098921dd520debf78b51dc1f61d
Yash Joshi <yash@dropbase.io>
2023-11-17 00:25:48 +0530

    add status bar

--- client/src/layout/DashboardLayout.tsx
+++ client/src/layout/DashboardLayout.tsx
@@ -1,26 +1,33 @@
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, Stack } from '@chakra-ui/react';
 import { PropsWithChildren } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import { Navbar } from './Navbar';
+import { StatusBar } from './StatusBar';
 
 export const DashboardLayout = ({ children }: PropsWithChildren<any>) => {
 	const { pathname } = useLocation();
 
-	const shouldNotDisplayNavbar =
-		pathname.startsWith('/apps/') ||
+	const loginRoutes =
 		pathname.startsWith('/login') ||
 		pathname.startsWith('/register') ||
 		pathname.startsWith('/reset');
 
+	const shouldNotDisplayNavbar = pathname.startsWith('/apps/') || loginRoutes;
+
 	return (
-		<Box height="100vh" overflow="hidden" position="relative">
-			<Flex h="full">
+		<Stack spacing="0" height="100vh" overflow="hidden" position="relative">
+			<Flex flex="1">
 				{shouldNotDisplayNavbar ? null : <Navbar />}
 				<Box overflowY="auto" flex="1" p="0">
 					{children}
 				</Box>
 			</Flex>
-		</Box>
+			{loginRoutes ? null : (
+				<Box maxH="20px" flexShrink="0">
+					<StatusBar />
+				</Box>
+			)}
+		</Stack>
 	);
 };
---
+++ client/src/layout/StatusBar.tsx
@@ -0,0 +1,45 @@
+import { useQuery } from 'react-query';
+import { Circle, Stack, Text } from '@chakra-ui/react';
+import { workerAxios } from '../lib/axios';
+
+export const STATUS_QUERY_KEY = 'allFiles';
+
+const fetchStatus: any = async () => {
+	const response = await workerAxios.get<any>(`/docs/`);
+
+	return response.data;
+};
+
+const useStatus = () => {
+	const queryKey = [STATUS_QUERY_KEY];
+
+	const { data: response, ...rest } = useQuery(queryKey, () => fetchStatus(), {
+		refetchInterval: 1 * 1000,
+		refetchIntervalInBackground: true,
+	});
+
+	return {
+		...rest,
+		queryKey,
+	};
+};
+
+export const StatusBar = () => {
+	const { isLoading } = useStatus();
+
+	return (
+		<Stack
+			direction="row"
+			spacing="1"
+			alignItems="center"
+			fontWeight="medium"
+			p="1"
+			h="full"
+			bg="white"
+			borderTopWidth="1px"
+		>
+			<Circle size="2" bg="green" />
+			<Text fontSize="xs">Connected</Text>
+		</Stack>
+	);
+};
