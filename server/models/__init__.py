7f8933b36e2922bc68b4a72a564708b7ce68a960
Ayazhan Zhakhan <az@dropbase.io>
2023-12-04 14:32:44 -0600

    added health report endpoint

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -6,6 +6,7 @@ from .files import Files
 from .group import Group
 from .page import Page
 from .policy import Policy
+from .reset_token import ResetToken
 from .role import Role
 from .tables import Tables
 from .token import Token
@@ -13,5 +14,5 @@ from .user import User
 from .user_group import UserGroup
 from .user_role import UserRole
 from .widget import Widget
+from .worker_status import WorkerStatus
 from .workspace import Workspace
-from .reset_token import ResetToken
