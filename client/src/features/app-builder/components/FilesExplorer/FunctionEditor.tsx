041a1255f9801f52a62731ce330b5332d4dbc109
jonDropbase <jon@dropbase.io>
2023-12-05 12:11:07 -0500

    Patch: Allow python code to be manually saved

--- client/src/features/app-builder/components/FilesExplorer/FunctionEditor.tsx
+++ client/src/features/app-builder/components/FilesExplorer/FunctionEditor.tsx
@@ -14,12 +14,14 @@ import { useAtomValue } from 'jotai';
 import { useEffect, useState } from 'react';
 import { Save } from 'react-feather';
 import { useQueryClient } from 'react-query';
+import { useToast } from '@/lib/chakra-ui';
 
 import { useParams } from 'react-router-dom';
 import { usePythonEditor } from '@/components/Editor';
 import { COLUMN_PROPERTIES_QUERY_KEY, useFile, usePageFiles } from '@/features/app-builder/hooks';
 import { pageAtom, useGetPage } from '@/features/page';
-
+import { useSaveCode } from '@/features/app-builder/hooks';
+import { getErrorMessage } from '@/utils';
 import { FunctionTerminal } from './FunctionTerminal';
 import { TABLE_DATA_QUERY_KEY } from '../../../smart-table/hooks';
 import { findFunctionDeclarations } from '../../utils';
@@ -42,7 +44,7 @@ const PythonEditorLSP = ({ code: defaultCode, filePath, updateCode }: any) => {
 export const FunctionEditor = ({ id }: any) => {
 	const queryClient = useQueryClient();
 	const { pageName, appName } = useAtomValue(pageAtom);
-
+	const toast = useToast();
 	const { pageId } = useParams();
 	const { files } = useGetPage(pageId);
 
@@ -55,7 +57,7 @@ export const FunctionEditor = ({ id }: any) => {
 	const fileName = file ? `${file?.name}${file?.type === 'sql' ? '.sql' : '.py'}` : null;
 
 	const filePath = workerFiles.find((f: any) => f.endsWith(fileName));
-	const { isLoading, code } = useFile({
+	const { isLoading, code, refetch } = useFile({
 		pageName,
 		appName,
 		fileName,
@@ -63,11 +65,41 @@ export const FunctionEditor = ({ id }: any) => {
 
 	const [updatedCode, setCode] = useState(code || '');
 
+	const savePythonMutation = useSaveCode({
+		onSuccess: () => {
+			toast({
+				status: 'success',
+				title: 'Updated code',
+			});
+			refetch();
+		},
+		onError: (error: any) => {
+			toast({
+				status: 'error',
+				title: 'Failed to update code',
+				description: getErrorMessage(error),
+			});
+		},
+	});
+
+	const handleSave = () => {
+		savePythonMutation.mutate({
+			pageName,
+			appName,
+			fileName,
+			sql: updatedCode,
+			// source: selectedSource,
+			fileId: id,
+			fileType: file?.type,
+		});
+	};
+
 	useEffect(() => {
 		setCode(code);
 	}, [id, code]);
 
 	const refetchColumns = () => {
+		handleSave();
 		queryClient.invalidateQueries(TABLE_DATA_QUERY_KEY);
 		queryClient.invalidateQueries(COLUMN_PROPERTIES_QUERY_KEY);
 	};
@@ -93,21 +125,21 @@ export const FunctionEditor = ({ id }: any) => {
 
 	return (
 		<Stack h="full" bg="white" spacing="0" divider={<Divider />} w="full">
-			{file?.type === 'data_fetcher' ? (
-				<Stack p="2" direction="row" alignItems="center" justifyContent="end">
-					<Button
-						w="fit-content"
-						onClick={refetchColumns}
-						variant="outline"
-						colorScheme="gray"
-						size="sm"
-						isDisabled={code === updatedCode}
-						leftIcon={<Save size="14" />}
-					>
-						Update
-					</Button>
-				</Stack>
-			) : null}
+			{/* {file?.type === 'data_fetcher' ? ( */}
+			<Stack p="2" direction="row" alignItems="center" justifyContent="end">
+				<Button
+					w="fit-content"
+					onClick={refetchColumns}
+					variant="outline"
+					colorScheme="gray"
+					size="sm"
+					isDisabled={code === updatedCode}
+					leftIcon={<Save size="14" />}
+				>
+					Update
+				</Button>
+			</Stack>
+			{/* ) : null} */}
 
 			{!isLoadingWorkerFiles && updatedCode && isNotSameFunctionName ? (
 				<Alert flexShrink="0" status="error">
