4e28e35646ebd0f0f072b99698ded47a335e811d
jonDropbase <jon@dropbase.io>
2023-12-19 16:45:18 -0500

    Remove strict mode to allow react-beautiful-dnd to work

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { Dashboard } from './Dashboard';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
-		<Dashboard />
-	</React.StrictMode>,
+	// <React.StrictMode>
+	<Dashboard />,
+	// </React.StrictMode>,
 );
