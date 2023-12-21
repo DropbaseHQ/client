38a1a9d7c1406436e527b30dcabc822a8e49a84d
jonDropbase <jon@dropbase.io>
2023-12-21 16:01:17 -0500

    Adjust page atom for widgets attribute

--- client/src/features/page/atoms/index.ts
+++ client/src/features/page/atoms/index.ts
@@ -1,7 +1,13 @@
 import { atom } from 'jotai';
 
-export const pageAtom = atom({
+export const pageAtom = atom<{
+	widgetId: string | null;
+	appName: string | null;
+	pageName: string | null;
+	widgets: string[] | null;
+}>({
 	widgetId: null,
 	appName: null,
 	pageName: null,
+	widgets: [],
 });
