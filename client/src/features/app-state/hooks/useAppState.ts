2daff8db805057eeea1b26edff27da72a944ad25
Ayazhan Zhakhan <az@dropbase.io>
2024-01-09 18:10:35 -0500

    updated endpoint that pulls page data

--- client/src/features/app-state/hooks/useAppState.ts
+++ client/src/features/app-state/hooks/useAppState.ts
@@ -6,9 +6,8 @@ import { workerAxios } from '@/lib/axios';
 export const APP_STATE_QUERY_KEY = 'appState';
 
 
-// TODO: @yash-dropbase please review, removed from backend
 const fetchAppState = async ({ appName, pageName }: { appName: string; pageName: string }) => {
-	const response = await workerAxios.get<any>(`/sync/${appName}/${pageName}`);
+	const response = await workerAxios.get<any>(`/page/${appName}/${pageName}`);
 	return response.data;
 };
 
