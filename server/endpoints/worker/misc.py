39c5c58a4a1a3884df7d6edd03d62cb2938b87a4
jonDropbase <jon@dropbase.io>
2024-01-10 16:51:10 -0500

    Add verify token endpoint for worker

--- server/endpoints/worker/misc.py
+++ server/endpoints/worker/misc.py
@@ -4,6 +4,8 @@ from sqlalchemy.orm import Session
 from server import crud
 from server.controllers.tables.convert import call_gpt, fill_smart_cols_data
 from server.schemas.tables import ConvertTable, ReadTables, UpdateSmartTables
+from server.utils.authorization import get_current_user
+
 from server.utils.connect import get_db
 from server.utils.state_context import get_state_context_payload
 
@@ -32,3 +34,11 @@ def update_smart_columns(req: UpdateSmartTables, db: Session = Depends(get_db)):
     page = crud.page.get_table_page(db, table_id=table.id)
     state_context = get_state_context_payload(db, page.id)
     return {"state_context": state_context, "table": table}
+
+
+@router.post(
+    "/verify_token",
+    dependencies=[Depends(get_current_user)],
+)
+def verify_token():
+    return {"success": True}
