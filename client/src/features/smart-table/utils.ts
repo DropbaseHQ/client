ba11cc749f6ae58c8df17e88ef34cb4c015966ad
Param Thakkar <param@dropbase.io>
2024-01-17 12:22:31 -0500

    Use date-fns library to parse time

--- client/src/features/smart-table/utils.ts
+++ client/src/features/smart-table/utils.ts
@@ -1,4 +1,4 @@
-import { format } from 'date-fns';
+import { format, parse } from 'date-fns';
 
 // converts epoch time to string of format yyyy-mm-dd
 export const formatDate = (epoch: number) => {
@@ -7,14 +7,8 @@ export const formatDate = (epoch: number) => {
 
 // converts hh:mm:ss.ms to hh:mm:ss AM/PM
 export const formatTime = (time: string) => {
-	let [hours, mins, secs] = time.split(':');
-	const suffix = parseInt(hours, 10) >= 12 ? 'PM' : 'AM';
-	hours = String(parseInt(hours, 10) % 12).padStart(2, '0');
-	if (hours === '00') hours = '12';
-	mins = String(Math.round(parseInt(mins, 10))).padStart(2, '0');
-	secs = String(Math.round(parseInt(secs, 10))).padStart(2, '0');
-
-	return `${hours}:${mins}:${secs} ${suffix}`;
+	const date = parse(time.split('.')[0], 'HH:mm:ss', new Date());
+	return format(date, 'hh:mm:ss a');
 };
 
 // converts epoch time to string of format yyyy-mm-dd hh:mm:ss AM/PM
