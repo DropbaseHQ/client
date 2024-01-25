37ed8e354228f5ff0064a20f2d47841844557c3e
Yash Joshi <yash@dropbase.io>
2024-01-26 00:29:44 +0530

    add condition to use different entry point for app

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
+import VitePluginHtmlEnv from 'vite-plugin-html-env';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		VitePluginHtmlEnv({
+			compiler: true,
+		}),
+		react(),
+	],
 	server: {
 		port: 3030,
 		open: '/',
