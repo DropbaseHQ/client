000f544b4a8fc1d0669437d7fb3aabe61e6f6171
jonDropbase <jon@dropbase.io>
2024-01-25 15:33:21 -0500

    Add worker axios interceptor for worker-sl-token

--- client/src/lib/axios/index.ts
+++ client/src/lib/axios/index.ts
@@ -63,3 +63,31 @@ axios.interceptors.response.use(
 		return Promise.reject(err);
 	},
 );
+
+workerAxios.interceptors.response.use(
+	(res) => {
+		return res;
+	},
+	async (err) => {
+		const apiConfig = err.config;
+		if (err.response.status === 401 && !apiConfig.retry) {
+			apiConfig.retry = true;
+			try {
+				return await axios(apiConfig);
+			} catch (_error) {
+				if (
+					!(
+						window.location.pathname.includes('/login') ||
+						window.location.pathname.includes('/register') ||
+						window.location.pathname.includes('/email-confirmation') ||
+						window.location.pathname.includes('/forgot') ||
+						window.location.pathname.includes('/reset')
+					)
+				) {
+					window.location.href = '/login';
+				}
+			}
+		}
+		return Promise.reject(err);
+	},
+);
