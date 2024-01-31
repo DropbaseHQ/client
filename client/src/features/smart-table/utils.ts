b5effd7f21aa055bbd4cb4bc4eca90dddeb984e5
Param Thakkar <param@dropbase.io>
2024-01-31 16:00:48 -0500

    Don't crash on null dates

--- client/src/features/smart-table/utils.ts
+++ client/src/features/smart-table/utils.ts
@@ -5,16 +5,28 @@ const convertToUTC = (epoch: string) =>
 
 // converts epoch time to string of format yyyy-mm-dd
 export const formatDate = (epoch: string) => {
-	return format(convertToUTC(epoch), 'yyyy-MM-dd');
+	try {
+		return format(convertToUTC(epoch), 'yyyy-MM-dd');
+	} catch (e) {
+		return epoch;
+	}
 };
 
 // converts hh:mm:ss.ms to hh:mm:ss AM/PM
 export const formatTime = (time: string) => {
-	const date = parse(time.split('.')[0], 'HH:mm:ss', new Date());
-	return format(date, 'hh:mm:ss a');
+	try {
+		const date = parse(time.split('.')[0], 'HH:mm:ss', new Date());
+		return format(date, 'hh:mm:ss a');
+	} catch (e) {
+		return time;
+	}
 };
 
 // converts epoch time to string of format yyyy-mm-dd hh:mm:ss AM/PM
 export const formatDateTime = (epoch: string) => {
-	return format(convertToUTC(epoch), 'yyyy-MM-dd hh:mm:ss a');
+	try {
+		return format(convertToUTC(epoch), 'yyyy-MM-dd hh:mm:ss a');
+	} catch (e) {
+		return epoch;
+	}
 };
