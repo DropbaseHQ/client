63f037d9a2757e3a4489d301a2213db437bbc4c1
jonDropbase <jon@dropbase.io>
2024-02-05 17:44:37 -0500

    Auth no longer uses cookies

--- server/utils/authentication.py
+++ server/utils/authentication.py
@@ -13,7 +13,6 @@ from server.utils.connect import get_db
 class Settings(BaseModel):
     authjwt_secret_key: str = "secret"
     # Configure application to store and get JWT from cookies
-    authjwt_token_location: set = {"cookies"}
     # Disable CSRF Protection for this example. default is True
     authjwt_cookie_csrf_protect: bool = False
     authjwt_cookie_secure: bool = True
