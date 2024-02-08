b304ddd69f2d56608e095ceb7ec13481c546e2a1
Param Thakkar <param@dropbase.io>
2024-02-08 15:31:03 -0500

    Add column_type to filters in tableData req

--- client/src/features/smart-table/hooks/current-table.ts
+++ client/src/features/smart-table/hooks/current-table.ts
@@ -20,14 +20,6 @@ export const useCurrentTableData = (tableName: any) => {
 	const { pageName, appName } = useParams();
 	const setTableColumnTypes = useSetAtom(tableColumnTypesAtom);
 
-	const allFilters = useAtomValue(filtersAtom);
-	const filters = (allFilters[tableName] || []).filter((f: any) => {
-		if (f.condition === 'is null' || f.condition === 'is not null') {
-			return f.column_name;
-		}
-		return f.column_name && f.value;
-	});
-
 	const allSorts = useAtomValue(sortsAtom);
 	const sorts = (allSorts[tableName] || []).filter((f: any) => f.column_name);
 
@@ -35,6 +27,28 @@ export const useCurrentTableData = (tableName: any) => {
 		currentPage: 0,
 		pageSize: DEFAULT_PAGE_SIZE,
 	};
+
+	const { tables, isLoading: isLoadingPage } = useGetPage({ appName, pageName });
+	const table = tables?.find((t: any) => t.name === tableName);
+	const columnDict = table?.columns?.reduce((agg: any, c: any) => {
+		return {
+			...agg,
+			[c.name]: c,
+		};
+	}, {});
+
+	const allFilters = useAtomValue(filtersAtom);
+	const filters = (allFilters[tableName] || [])
+		.filter((f: any) => {
+			if (f.condition === 'is null' || f.condition === 'is not null') {
+				return f.column_name;
+			}
+			return f.column_name && f.value;
+		})
+		.map((f: any) => ({
+			...f,
+			column_type: columnDict[f?.column_name]?.display_type || '',
+		}));
 
 	const tableData = useTableData({
 		tableName,
@@ -45,14 +59,6 @@ export const useCurrentTableData = (tableName: any) => {
 		...pageInfo,
 	});
 
-	const { tables, isLoading: isLoadingPage } = useGetPage({ appName, pageName });
-	const table = tables?.find((t: any) => t.name === tableName);
-	const columnDict = table?.columns?.reduce((agg: any, c: any) => {
-		return {
-			...agg,
-			[c.name]: c,
-		};
-	}, {});
 	setTableColumnTypes((old: any) => {
 		const newColumnTypes: any = {};
 		Object.entries(columnDict || {}).forEach(([key, value]: any) => {
