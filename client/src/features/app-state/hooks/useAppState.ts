3cecbcf516c03a3acc03d1db869ec4c219cbd8fd
jonDropbase <95246696+jonDropbase@users.noreply.github.com>
2024-02-12 15:05:45 -0500

    Improvement: Handle permissions around edit and preview on client (#193)
    * Add redirection when getting 403 on app click
    
    * Hide edit button
    
    * If navigating to edit w/o permissions, redirect to studio
    
    * Remove worker cookie on logout or login
    
    * Clear react query cache on login and logout

--- client/src/features/app-state/hooks/useAppState.ts
+++ client/src/features/app-state/hooks/useAppState.ts
@@ -1,17 +1,21 @@
 import { useQuery } from 'react-query';
 import { useMemo } from 'react';
-
+import { useNavigate } from 'react-router-dom';
+import { FetchPageResponse } from '@/features/page';
 import { workerAxios } from '@/lib/axios';
+import { useToast } from '@/lib/chakra-ui';
 
 export const APP_STATE_QUERY_KEY = 'appState';
 
 const fetchAppState = async ({ appName, pageName }: { appName: string; pageName: string }) => {
-	const response = await workerAxios.get<any>(`/page/${appName}/${pageName}`);
+	const response = await workerAxios.get<FetchPageResponse>(`/page/${appName}/${pageName}`);
 	return response.data;
 };
 
 export const useAppState = (appName: string, pageName: string) => {
 	const queryKey = [APP_STATE_QUERY_KEY, appName, pageName];
+	const toast = useToast();
+	const navigate = useNavigate();
 
 	const { data: response, ...rest } = useQuery(
 		queryKey,
@@ -21,10 +25,22 @@ export const useAppState = (appName: string, pageName: string) => {
 			refetchInterval: false,
 		},
 	);
-
+	if (rest?.error) {
+		const errorStatusCode = rest.error?.response?.status;
+		if (errorStatusCode === 403) {
+			toast.closeAll();
+			toast({
+				title: 'Unauthorized',
+				description: 'You do not have permission to view this page.',
+				status: 'error',
+			});
+			navigate('/apps');
+		}
+	}
 	const info = useMemo(() => {
 		return {
-			state: response || { context: {}, state: {} },
+			state: response?.state_context || { context: {}, state: {} },
+			permissions: response?.permissions,
 		};
 	}, [response]);
 
