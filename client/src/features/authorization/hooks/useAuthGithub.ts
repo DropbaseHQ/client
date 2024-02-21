c4ee3bd2205a0bb73369b3d97f0810c798dc650f
jonDropbase <jon@dropbase.io>
2024-02-21 15:31:35 -0500

    Add new hook for github auth

---
+++ client/src/features/authorization/hooks/useAuthGithub.ts
@@ -0,0 +1,34 @@
+import { useQuery } from 'react-query';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { axios } from '@/lib/axios';
+
+export type GithubAuthResponse = {
+	access_token: string;
+	refresh_token: string;
+};
+const authorizeGithub = async (code: string) => {
+	const response = await axios.get<GithubAuthResponse>(`/user/github_auth/${code}`);
+
+	return response.data;
+};
+
+export const useAuthorizeGithub = () => {
+	const { search } = useLocation();
+	const params = new URLSearchParams(search);
+	const code = params.get('code');
+
+	const navigate = useNavigate();
+	return useQuery(['github_auth', code], () => authorizeGithub(code || ''), {
+		enabled: !!code,
+		onSettled: () => {
+			// navigate('/login');
+		},
+		onSuccess: (data: any) => {
+			if (data?.access_token && data?.refresh_token) {
+				localStorage.setItem('access_token', data.access_token);
+				localStorage.setItem('refresh_token', data.refresh_token);
+				navigate('/apps');
+			}
+		},
+	});
+};
