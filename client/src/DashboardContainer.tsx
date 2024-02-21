63f158861361d0102d8c6e69bde7e479df7a9998
Param Thakkar <param@dropbase.io>
2024-02-21 12:38:39 -0500

    merge artifacts

--- client/src/DashboardContainer.tsx
+++ client/src/DashboardContainer.tsx
@@ -5,15 +5,20 @@ import { ReactQueryDevtools } from 'react-query/devtools';
 
 import { theme } from '@/lib/chakra-ui';
 import { queryClient } from '@/lib/react-query';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+const GOOGLE_CLIENT_ID = '29567616605-al7naomojd9n3hhtt7gdtkmm7vggsb0m.apps.googleusercontent.com';
 
 export const DashboardContainer = ({ children }: any) => {
 	return (
 		<BrowserRouter>
 			<ChakraProvider theme={theme}>
-				<QueryClientProvider client={queryClient}>
-					{children}
-					<ReactQueryDevtools position="bottom-right" />
-				</QueryClientProvider>
+				<GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+					<QueryClientProvider client={queryClient}>
+						{children}
+						<ReactQueryDevtools position="bottom-right" />
+					</QueryClientProvider>
+				</GoogleOAuthProvider>
 			</ChakraProvider>
 		</BrowserRouter>
 	);
