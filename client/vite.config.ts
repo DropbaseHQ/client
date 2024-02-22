626a2809ad924985ea54691c2ef158e96a281ab2
Yash Joshi <yash@dropbase.io>
2024-02-22 20:21:12 +0530

    split build into multiple bundles

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import VitePluginHtmlEnv from 'vite-plugin-html-env';
+import { visualizer } from 'rollup-plugin-visualizer';
+
+const VISUALIZER = false;
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +13,48 @@ export default defineConfig({
 			compiler: true,
 		}),
 		react(),
+		splitVendorChunkPlugin(),
+
+		VISUALIZER &&
+			visualizer({
+				open: true,
+				gzipSize: true,
+				filename: 'chunks-report.html',
+			}),
 	],
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks(id: string) {
+					// creating a chunk to @open-ish deps. Reducing the vendor chunk size
+					if (id.includes('monaco')) {
+						return '@monaco';
+					}
+
+					if (id.includes('vscode')) {
+						return '@vscode';
+					}
+
+					if (id.includes('chakra')) {
+						return '@chakra';
+					}
+
+					if (id.includes('glideapps')) {
+						return '@glide-grid';
+					}
+
+					// creating a chunk to react routes deps. Reducing the vendor chunk size
+					if (
+						id.includes('react-router-dom') ||
+						id.includes('@remix-run') ||
+						id.includes('react-router')
+					) {
+						return '@react-router';
+					}
+				},
+			},
+		},
+	},
 	server: {
 		port: 3030,
 		open: '/',
