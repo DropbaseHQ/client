c14af52963ffbbed0d41bde8ea88940218c78af0
jonDropbase <jon@dropbase.io>
2024-02-26 16:18:51 -0500

    Navigate to login in github auth

--- client/src/features/authorization/GithubAuth.tsx
+++ client/src/features/authorization/GithubAuth.tsx
@@ -1,8 +1,16 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Text, Flex, Progress, Box } from '@chakra-ui/react';
 import { useAuthorizeGithub } from './hooks/useAuthGithub';
 
 export const GithubAuth = () => {
-	useAuthorizeGithub();
+	const { isLoading } = useAuthorizeGithub();
+	const navigate = useNavigate();
+	useEffect(() => {
+		if (!isLoading) {
+			navigate('/login');
+		}
+	}, [isLoading, navigate]);
 	return (
 		<Flex h="100%" w="100%" alignItems="center" justifyContent="center" direction="column">
 			<Text>Authenticating with Github</Text>
