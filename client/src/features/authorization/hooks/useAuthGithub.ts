3b1f7460ea3e7623d63d0f5dcef5b5235dca9d7e
jonDropbase <jon@dropbase.io>
2024-02-26 16:12:02 -0500

    Add error toasting

--- client/src/features/authorization/hooks/useAuthGithub.ts
+++ client/src/features/authorization/hooks/useAuthGithub.ts
@@ -1,5 +1,8 @@
 import { useQuery } from 'react-query';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { useToast } from '@/lib/chakra-ui';
+import { getErrorMessage } from '@/utils';
+
 import { axios } from '@/lib/axios';
 
 export type GithubAuthResponse = {
@@ -13,6 +16,7 @@ const authorizeGithub = async (code: string) => {
 };
 
 export const useAuthorizeGithub = () => {
+	const toast = useToast();
 	const { search } = useLocation();
 	const params = new URLSearchParams(search);
 	const code = params.get('code');
@@ -20,15 +24,21 @@ export const useAuthorizeGithub = () => {
 	const navigate = useNavigate();
 	return useQuery(['github_auth', code], () => authorizeGithub(code || ''), {
 		enabled: !!code,
-		onSettled: () => {
-			// navigate('/login');
-		},
+
 		onSuccess: (data: any) => {
 			if (data?.access_token && data?.refresh_token) {
 				localStorage.setItem('access_token', data.access_token);
 				localStorage.setItem('refresh_token', data.refresh_token);
 				navigate('/apps');
 			}
+		},
+		onError: (error) => {
+			toast({
+				title: 'Github Auth Failed',
+				status: 'error',
+				description: getErrorMessage(error),
+			});
+			navigate('/login');
 		},
 	});
 };
