f3e9b88107a592d10b4a113e503cc50722a9d5de
Param Thakkar <param@dropbase.io>
2024-02-28 14:12:51 -0500

    Move OnboardingForm to DashboardRoutes

--- client/src/features/app/routes/index.tsx
+++ client/src/features/app/routes/index.tsx
@@ -2,7 +2,6 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { AppRoutes } from './AppRoutes';
 import { AppList } from '@/features/app-list';
 import { useSyncProxyToken } from '@/features/settings/hooks/token';
-import { OnboardingForm } from '@/features/authorization/OnboardingForm';
 
 export const App = () => {
 	const { hasTokens, isLoading } = useSyncProxyToken();
@@ -12,7 +11,6 @@ export const App = () => {
 	}
 	return (
 		<>
-			<OnboardingForm />
 			<Routes>
 				<Route index element={<AppList />} />
 				<Route path=":appName/:pageName/*" element={<AppRoutes />} />
