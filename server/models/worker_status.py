c82e3a1beeef0ab4699b6fbe8a558c70b3d01c88
jonDropbase <jon@dropbase.io>
2024-02-28 18:31:49 -0500

    Refactor: WorkerStatus model to include UUID as primary key.

--- server/models/worker_status.py
+++ server/models/worker_status.py
@@ -1,5 +1,6 @@
 from sqlalchemy import TIMESTAMP, Column, ForeignKey, PrimaryKeyConstraint, String
-from sqlalchemy.sql import func
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.sql import func, text
 
 from .base import Base
 
@@ -8,6 +9,7 @@ class WorkerStatus(Base):
     token = Column(String, ForeignKey("token.token", ondelete="SET NULL"))
     version = Column(String)
     date = Column(TIMESTAMP, server_default=func.now())
-
-    __table_args__ = (PrimaryKeyConstraint("token", "date"),)
+    id = Column(
+        UUID(as_uuid=True), server_default=text("uuid_generate_v4()"), primary_key=True
+    )
     __tablename__ = "worker_status"
