a08870b7462baa1745819d7495f48f6781712eb9
stephen-turtles <stephen@dropbase.io>
2024-03-18 14:25:18 -0400

    more consistent results, fixed issues for postgres

--- server/controllers/tables/gpt_templates/mysql_gpt_template.py
+++ server/controllers/tables/gpt_templates/mysql_gpt_template.py
@@ -16,13 +16,14 @@ Sample JSON object value
 }}
 ```
 This is only a sample value, only name the database_name "public" if it is actually public. 
-You can find the actual database_names to fill into the "database_name" under the key "database" write after "metadata"
+This part is extremely important please be careful. When trying to find the "database_name" ENSURE that the database_name consists of the table that is queried in user_query.
+Loop through all the database_names under the values under the "database" key until you find a key (under the database key) that is titled the same as the table in the query, when you find it assign that database to be the database_name
+If you absolutely can not find a match then use the "default_schema" under the "meta_data" key
 
 In the sample JSON, "output column" is the column name as it would be output when executing the SQL statement.
 You will be provided a list of column names that will be returned by the query. Your job is to, for each column name, determine its database_name, table_name, and column_name based on the SQL query. You may only return information on the columns specified in Column names. You must return information on each of the columns specified in Column names.
 "table" is the table name and "column" is the column name, both are inferred from the SQL query.
 "database" is the database, this can be found by referencing the Database schema.
-If not otherwise specified, use the default database specified in the Database schema metadata.
 
 Make sure you include each and every column referenced in the SQL statement. Do not miss any columns. When in doubt, make a guess about which column is part of which table based on your understanding of the world and web application building to make sure all columns are covered.
 
